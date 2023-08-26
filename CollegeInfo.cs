ef1cbe73a69f708b1902d6e238fb7f1016442374
Smriti1208 <109757185+Smriti1208@users.noreply.github.com>
2023-08-26 12:14:12 +0530

    Add files via upload

---
+++ CollegeInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+	class CollegeInfo
+	{
+
+		public static void CollegeDet()
+		{
+			// Get user information
+			Console.Write("Enter your name: ");
+			string userName = Console.ReadLine();
+
+			// Get college information
+			Console.Write("Enter college name: ");
+			string collegeName = Console.ReadLine();
+
+			Console.Write("Enter college strength: ");
+			int collegeStrength = int.Parse(Console.ReadLine());
+
+			// Display the collected information
+			Console.WriteLine("\nUser Information:");
+			Console.WriteLine($"Name: {userName}");
+
+			Console.WriteLine($"College Name: {collegeName}");
+			Console.WriteLine($"College Strength: {collegeStrength}");
+
+
+		}
+		static void Main()
+		{
+			CollegeDet();
+			Console.WriteLine("\nThank you for providing your information!");
+		}
+	}
+}
---
+++ LoanCal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+	internal class LoanCal
+	{
+		public static void Main()
+		{
+			Console.Write("Enter The total amount ");
+			double Principle = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter the tenure (in years): ");
+			double tenure = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter rate of interest (%): ");
+			double interestRate = double.Parse(Console.ReadLine());
+
+			// Calculate simple interest
+			double simpleInterest = (Principle * interestRate * tenure) / 100;
+
+	
+			Console.WriteLine($"Loan Amount: {Principle}");
+			Console.WriteLine($"Tenure: {tenure} years");
+			Console.WriteLine($"Interest Rate: {interestRate}%");
+			Console.WriteLine($"Simple Interest: {simpleInterest}");
+			
+
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
