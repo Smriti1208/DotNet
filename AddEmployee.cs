ae30d196130ff08566e20645bc6e4e55a25cbb1e
Smriti1208 <109757185+Smriti1208@users.noreply.github.com>
2023-08-26 22:46:43 +0530

    Add files via upload

---
+++ AddEmployee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+	internal class AddEmployee
+	{
+		static void Main(string[] args)
+		{
+			List<string> employees = new List<string>();
+
+			// Add employees to the list
+			employees.Add("Alice");
+			employees.Add("Bob");
+			employees.Add("Charlie");
+			employees.Add("David");
+
+			// Display the names of employees
+			Console.WriteLine("List of Employees:");
+			foreach (string employee in employees)
+			{
+				Console.WriteLine(employee);
+			}
+		}
+	}
+}
+
+
+
+
+
+
